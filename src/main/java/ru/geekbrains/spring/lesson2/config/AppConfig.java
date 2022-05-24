82282f3b5372088851a8f0a007197f8147563abb
sujane <sujane_mf@mail.ru>
2022-05-24 17:22:01 +0300

    1. Есть класс Product (id, название, цена). Товары хранятся в бине ProductRepository, в виде List<Product>, при старте в него нужно добавить 5 любых товаров. 2. ProductRepository позволяет получить весь список или один товар по id. Создаем бин Cart, в который можно добавлять и удалять товары по id. 3. Написать консольное приложение, позволяющее управлять корзиной. 4. При каждом запросе корзины из контекста, должна создаваться новая корзина.

---
+++ src/main/java/ru/geekbrains/spring/lesson2/config/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.spring.lesson2.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.geekbrains.spring.lesson2")
+public class AppConfig {
+}
